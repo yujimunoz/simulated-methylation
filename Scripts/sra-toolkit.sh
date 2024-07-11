963da79ed8bfeb4dba19ee10a6c8d4195322f2a6
yujimunoz <149984439+yujimunoz@users.noreply.github.com>
2024-07-11 13:38:07 +0800

    Update sra-toolkit.sh

--- Scripts/sra-toolkit.sh
+++ Scripts/sra-toolkit.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 ~/Software/sratoolkit.3.1.1-ubuntu64/bin/prefetch --option-file $1
+#This is for downloading the sequences using a .txt file.
 while read sra;
 do
 	~/Software/sratoolkit.3.1.1-ubuntu64/bin/fasterq-dump --split-files "$sra"
+ #This is for extracting the reads for each line in the .txt file.
 done < $1
