5e74407efcea2b673b0d6c727f38020d8e54f7cd
Yuji Munoz <yujimunoz@Yujis-MacBook-Air.local>
2024-07-16 11:24:01 +0800

    Genome Indexing and Read Simulation

---
+++ Scripts/Sherman_Tawilis.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+/home/marguelles/Software/Sherman-0.1.8/Sherman \
+--length 250 --number_of_seqs 100 \
+--genome_folder /home/marguelles/Yuji/Genomes/S_tawilis/ncbi_dataset/data/GCA_030264315.1/ \
+--paired_end --conversion_rate 20
