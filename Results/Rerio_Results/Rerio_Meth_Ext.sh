083c172c6855156c8f8069054b3910ccc9ebe6ea
Yuji Munoz <yujimunoz@Yujis-MacBook-Air.local>
2024-07-18 14:23:08 +0800

    Results of Methylation Extraction

---
+++ Results/Rerio_Results/Rerio_Meth_Ext.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+READS_LOCATION="/home/marguelles/Yuji/Simulated_Reads/Rerio_Sim_Reads/"
+#This sets the path of the read
+count=0
+array=($(ls *.bam))
+#This converts all listed bam files in the location of the reads into an array
+len=${#array[@]}
+#This gets the length the array containing all files
+while [ $len -gt $count ];
+do
+        bam="$READS_LOCATION${array[$count]}"
+	#echo $bam
+	/home/marguelles/Software/Bismark-0.22.3/bismark_methylation_extractor --comprehensive --bedGraph --cytosine_report --genome_folder /home/marguelles/Yuji/Genomes/S_tawilis/ncbi_dataset/data/GCA_030264315.1 $bam
+#This passes the pair of reads as arguments to Bismark
+        count=$(($count+1))
+done
+
---
+++ Results/Tawilis_Results/Tawilis_Align.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+SIMREADS_LOCATION="/home/marguelles/Yuji/Simulated_Reads/All_Sim_Reads/"
+count=0
+array=($(ls *.fastq))
+#This converts all listed files in the location of the reads into an array
+len=${#array[@]}
+#This gets the length the array containing all files
+while [ $len -gt $count ];
+do
+        read_1="$SIMREADS_LOCATION${array[$count]}"
+#This sets read_1 as the first file in the directory
+        read_2="$SIMREADS_LOCATION${array[$count+1]}"
+#This sets read_2 as the file in the directory after read_1
+	/home/marguelles/Software/Bismark-0.22.3/bismark /home/marguelles/Yuji/Genomes/S_tawilis/ncbi_dataset/data/GCA_030264315.1 --path_to_bowtie2 /home/marguelles/Software/bowtie2-2.5.1-linux-x86_64 -1 $read_1 -2 $read_2
+#This passes the pair of reads as arguments to bowtie2
+        count=$(($count+2))
+done
